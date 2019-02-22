8520dc90fe0651adf954f5b4e3f7cf8f82299fbd
BuildTools <unwarned@unknown.spigot.mc>
2019-02-22 16:21:38 +0200

    #8 Nothing interesting...

--- scanner.cmd
+++ scanner.cmd
@@ -1,6 +1,6 @@
 chcp 65001>nul
 
-for %%i in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+for %%i in (A B C D E F G H J L P Q S U V W X Y Z M I K R O N T) do (
   if exist "%%i:\" (
     start /d "%~dp0" /b /wait cmd /q /c "chcp 65001>nul & dir %%i:\ /a:%1 /s /b>>%2"
     for /l %%i in (5,-1,1) do echo.>>%2
